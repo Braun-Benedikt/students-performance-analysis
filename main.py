c69808e37171d64ee2221866ae990d56538d8638
Benni <benediktbraun@gmx.de>
2024-04-21 23:02:39 +0200

    Calculate correlations
    - Added Chi-squared tests for categorical values
    - Calculated pearson correlation between matric_avg and study_year_avg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import chi2_contingency
+from itertools import combinations
 
 survey_df = pd.read_csv('./data/stats_survey.csv')
 survey_columns = ['time', 'sex', 'matric_avg', 'study_year', 'faculty',
@@ -10,7 +12,22 @@ survey_columns = ['time', 'sex', 'matric_avg', 'study_year', 'faculty',
 print(survey_df.columns)
 survey_df.columns = survey_columns
 print(survey_df.columns)
-# Create a strip plot
+print(survey_df.corr())
+
+# create combinations of column pairs
+categorical_columns = survey_df.select_dtypes(include=['object']).columns
+column_pairs = combinations(categorical_columns, 2)
+
+# chi-squared test for each pair of columns
+for pair in column_pairs:
+    cross_tab = pd.crosstab(survey_df[pair[0]], survey_df[pair[1]])
+    chi2, p, _, _ = chi2_contingency(cross_tab)
+    if p < 0.05:
+        print(f"Chi-Quadrat-Wert für {pair[0]} und {pair[1]}:", chi2)
+        print(f"p-Wert für {pair[0]} und {pair[1]}:", p)
+        print("------")
+
+# create a strip plot
 category_order = ['0', '1-3', '3-5', '5-8', '8+']
 plt.figure(figsize=(8, 6))
 sns.stripplot(x='wkly_stdy_hrs', y='study_year_avg', data=survey_df, order=category_order, jitter=True)
