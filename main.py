d95e8ee8c60b0a7397bdf15aceac1aa51bc1d975
Benni <benediktbraun@gmx.de>
2024-05-04 21:59:51 +0200

    work-in-progress: Clustering
    - implemented kmeans clustering
    - created 2d scatter plots for selected columns

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
 from itertools import combinations
+from sklearn.cluster import KMeans
 
 survey_df = pd.read_csv('./data/stats_survey.csv')
 survey_columns = ['time', 'sex', 'matric_avg', 'study_year', 'faculty',
@@ -23,9 +23,9 @@ for pair in column_pairs:
     cross_tab = pd.crosstab(survey_df[pair[0]], survey_df[pair[1]])
     chi2, p, _, _ = chi2_contingency(cross_tab)
     if p < 0.05:
-        print(f"Chi-Quadrat-Wert für {pair[0]} und {pair[1]}:", chi2)
-        print(f"p-Wert für {pair[0]} und {pair[1]}:", p)
-        print("------")
+        print(f'Chi-Quadrat-Wert für {pair[0]} und {pair[1]}:', chi2)
+        print(f'p-Wert für {pair[0]} und {pair[1]}:', p)
+        print('------')
 
 for column in survey_df.select_dtypes(include=['object']).columns:
     if column != 'time':
@@ -71,7 +71,29 @@ for column in selected_columns:
     survey_df[column] = survey_df[column].map(ordinal_categories_map).fillna(survey_df[column])
 print(survey_df.corrwith(survey_df['study_year_avg']))
 
-# scatterplots of 2023 GPA and the selected columns
+# TODO check n_clusters
+# perform k-Means clustering
+kmeans = KMeans(n_clusters=3)
+survey_df['cluster'] = 0
+survey_df.dropna(subset=selected_columns)['cluster'] = kmeans.fit_predict(survey_df[selected_columns].dropna())
+
+# print cluster centers
+print("Cluster Centers:")
+print(kmeans.cluster_centers_)
+
+# TODO most/all 2 dimensional plots do not make sense here
+# scatter plots of all possible combinations painted by cluster
+for i, column1 in enumerate(selected_columns):
+    for j, column2 in enumerate(selected_columns):
+        if i < j:
+            plt.scatter(survey_df[column1], survey_df[column2], c=survey_df['cluster'], cmap='viridis')
+            plt.xlabel(column1)
+            plt.ylabel(column2)
+            plt.title(f'Scatterplot von {column1} vs. {column2}')
+            plt.colorbar(label='Cluster')
+            plt.show()
+
+# scatter plots of 2023 GPA and the selected columns
 for column in selected_columns:
     plt.scatter(survey_df[column], survey_df['study_year_avg'])
     plt.title(f'Impact of {column} on GPA')
